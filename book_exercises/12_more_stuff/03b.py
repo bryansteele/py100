b58f300b6d1bd3f4cf4e0948b0a8e0fd167a7183
Bryan Steele <46832583+bryansteele@users.noreply.github.com>
2024-04-16 02:58:22 +0000

    Completes Chapter Exercises 2nd round

--- book_exercises/12_more_stuff/03b.py
+++ book_exercises/12_more_stuff/03b.py
@@ -1,4 +1,7 @@
 def sum_of_squares(num1, num2):
+    def square(number):
+        return number * number
+
     return square(num1) + square(num2)
 
 print(sum_of_squares(3, 4))   # 25 (3 * 3 + 4 * 4)
