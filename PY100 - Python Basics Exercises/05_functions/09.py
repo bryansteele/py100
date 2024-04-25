a61f96fae00e5b7b2f9e9ea8ac17c86be57124f7
bryansteele <bryan@steele101>
2024-04-25 10:53:29 -0400

    Completes Functions Exercises

--- PY100 - Python Basics Exercises/05_functions/09.py
+++ PY100 - Python Basics Exercises/05_functions/09.py
@@ -1,6 +1,6 @@
-def extract_language(locale):
-    return locale.split('_')[0]
+def extract_region(locale):
+    return locale.split('.')[0].split('_')[1]
 
-print(extract_language('en_US.UTF-8'))      # en
-print(extract_language('en_GB.UTF-8'))      # en
-print(extract_language('ko_KR.UTF-16'))     # ko
+print(extract_region('en_US.UTF-8'))    # US
+print(extract_region('en_GB.UTF-8'))    # GB
+print(extract_region('ko_KR.UTF-16'))   # KR
